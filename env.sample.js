cbdcce53b5a24958fed9b0819d510b9340e48be1
Sylitas <johnluy1999@gmail.com>
2023-07-08 17:51:04 +0700

    update name

--- env.sample.js
+++ env.sample.js
@@ -1,41 +1,41 @@
 exports.employeeDetailInfo = {
-  "Đỗ Xuân Nam": { phone: "098.480.4988" },
-  "Ngô Văn Tân": { phone: "098.769.7275" },
-  "Đỗ Xuân Đức": { phone: "037.467.9686" },
-  "Phạm Thị Hiền": { phone: "094.983.0503" },
-  "Nguyễn Đình Khang": { phone: "097.775.9172" },
-  "Nguyễn Trung Hậu": { phone: "092.200.0555" },
-  "Lê Anh Thọ": { phone: "093.640.0707" },
-  "Nguyễn Văn Long": { phone: "098.212.8793" },
-  "Đặng Văn Tứ": { phone: "038.257.4568" },
-}
+  'Đỗ Xuân Nam': { phone: '098.480.4988' },
+  'Ngô Văn Tân': { phone: '098.769.7275' },
+  'Đỗ Xuân Đức': { phone: '037.467.9686' },
+  'Bùi Ngọc Hải': { phone: '035.854.0002' },
+  'Nguyễn Đình Khang': { phone: '097.775.9172' },
+  'Nguyễn Trung Hậu': { phone: '092.200.0555' },
+  'Lê Anh Thọ': { phone: '093.640.0707' },
+  'Nguyễn Văn Long': { phone: '098.212.8793' },
+  'Đặng Văn Tứ': { phone: '038.257.4568' },
+};
 
-exports.tobaccosType = {
-  "555 King Size": { price: 27566.36 },
-  "555 Signature": { price: 31916.36 },
-  "555 Silver": { price: 20405.45 },
-  "555 Slim": { price: 11268.18 },
-  "555 Sphere": { price: 23132.73 },
-  "Dim": { price: 32202.73 },
-  "CA Classic": { price: 18643.64 },
-  "CA Demi": { price: 10742.73 },
-  "CA Chill": { price: 14475.45 },
-  "Kent Nanotek": { price: 27517.27 },
-  "Kent Switch": { price: 27517.27 },
-  "7D Crafted": { price: 7327.27 },
-}
+exports.tobaccosType = [
+  '555 King Size',
+  '555 Signature',
+  '555 Silver',
+  '555 Slim',
+  '555 Sphere',
+  'Dim',
+  'CA Classic',
+  'CA Demi',
+  'CA Chill',
+  'Kent Nanotek',
+  'Kent Switch',
+  '7D Crafted',
+];
 
 exports.address = '31 - LK9, Dọc Bún 1 La Khê, Hà Đông, TP Hà Nội';
 
 exports.phoneNumber = '092.107.4444';
 
 exports.reportConfig = {
-  fontSize: "12px",
+  fontSize: '12px',
   logoConfig: {
-    wxh: "50",
-    spaceLeft: "70px"
+    wxh: '50',
+    spaceLeft: '70px',
   },
-  signatureHeigh: "60px"
+  signatureHeigh: '60px',
 };
 
 exports.vat = 0.1;
--- model/generateSingleFile.js
+++ model/generateSingleFile.js
@@ -1,7 +1,12 @@
 const XLSX = require('xlsx');
 const fs = require('fs');
 
-const { excelDateToStringDateFormat, formatDate, convertValuesNumericToString, toVietnamese } = require('./utils');
+const {
+  excelDateToStringDateFormat,
+  formatDate,
+  convertValuesNumericToString,
+  toVietnamese,
+} = require('./utils');
 
 const {
   tobaccosType,
@@ -9,25 +14,28 @@ const {
   address,
   phoneNumber,
   reportConfig,
-  vat
+  vat,
 } = require('../env');
 const { convertHTML2PDF } = require('./generatePDF');
 
 const generateData = (date, employeeInfo) => {
   try {
-    const sellInfo = employeeInfo.reduce((pre, cur) => {
-      const tobaccoName = cur['Tên nhãn hiệu'].trim();
-      pre[tobaccoName].receive = cur['Số lượng nhận'] || 0;
-      pre[tobaccoName].return = cur['Số lượng trả lại'] || 0;
-      pre[tobaccoName].sold = cur['Số lượng bán'] || 0;
-      pre[tobaccoName].total = Math.round(cur['Thành tiền giá hóa đơn'] || 0);
-      pre[tobaccoName].price = cur[' Giá hóa đơn chưa bao gồm VAT '] || 0;
+    const sellInfo = employeeInfo.reduce(
+      (pre, cur) => {
+        const tobaccoName = cur['Tên nhãn hiệu'].trim();
+        pre[tobaccoName].receive = cur['Số lượng nhận'] || 0;
+        pre[tobaccoName].return = cur['Số lượng trả lại'] || 0;
+        pre[tobaccoName].sold = cur['Số lượng bán'] || 0;
+        pre[tobaccoName].total = Math.round(cur['Thành tiền giá hóa đơn'] || 0);
+        pre[tobaccoName].price = cur[' Giá hóa đơn chưa bao gồm VAT '] || 0;
 
-      return pre;
-    }, Object.keys(tobaccosType).reduce((pre, cur) => {
-      pre[cur] = { receive: 0, return: 0, sold: 0, total: 0, price: tobaccosType[cur].price };
-      return pre;
-    }, {}));
+        return pre;
+      },
+      tobaccosType.reduce((pre, cur) => {
+        pre[cur] = { receive: 0, return: 0, sold: 0, total: 0, price: 0 };
+        return pre;
+      }, {})
+    );
 
     const employeeName = employeeInfo[0]['Tên nhân viên'];
 
@@ -39,20 +47,23 @@ const generateData = (date, employeeInfo) => {
       address,
       phone: phoneNumber,
       employeePhone: employeeDetailInfo[employeeName].phone,
-      total: Object.keys(sellInfo).reduce((pre, cur) => {
-        pre.receive += sellInfo[cur].receive;
-        pre.return += sellInfo[cur].return;
-        pre.sold += sellInfo[cur].sold;
-        pre.total += sellInfo[cur].total;
+      total: Object.keys(sellInfo).reduce(
+        (pre, cur) => {
+          pre.receive += sellInfo[cur].receive;
+          pre.return += sellInfo[cur].return;
+          pre.sold += sellInfo[cur].sold;
+          pre.total += sellInfo[cur].total;
 
-        return pre;
-      }, { receive: 0, return: 0, sold: 0, total: 0 }),
+          return pre;
+        },
+        { receive: 0, return: 0, sold: 0, total: 0 }
+      ),
       tobaccos: Object.keys(sellInfo).map((tobacco, index) => ({
         stt: index + 1,
         name: tobacco,
         info: convertValuesNumericToString(sellInfo[tobacco]),
-      }))
-    }
+      })),
+    };
 
     pdfData.taxVAT = Math.round(pdfData.total.total * vat);
 
@@ -61,11 +72,13 @@ const generateData = (date, employeeInfo) => {
     const moneyInVietnamese = toVietnamese(pdfData.totalPay);
 
     if (pdfData.totalPay !== 0) {
-      pdfData.inVietnamese = moneyInVietnamese.charAt(0).toUpperCase() + moneyInVietnamese.slice(1) + ' đồng';
+      pdfData.inVietnamese =
+        moneyInVietnamese.charAt(0).toUpperCase() +
+        moneyInVietnamese.slice(1) +
+        ' đồng';
     } else {
-      pdfData.inVietnamese = 'Không đồng'
+      pdfData.inVietnamese = 'Không đồng';
     }
-
 
     pdfData.taxVAT = pdfData.taxVAT.toLocaleString();
     pdfData.totalPay = pdfData.totalPay.toLocaleString();
@@ -73,21 +86,34 @@ const generateData = (date, employeeInfo) => {
 
     return pdfData;
   } catch (error) {
-    console.error('😎 Sylitas | An unexpected error when generating data', error);
+    console.error(
+      '😎 Sylitas | An unexpected error when generating data',
+      error
+    );
     throw error;
   }
-}
+};
 
 const generateTemplateHTML = (path = `${__dirname}/paymentReceipt.html`) => {
-  let htmlContent = fs.readFileSync(path, "utf8");
+  let htmlContent = fs.readFileSync(path, 'utf8');
   htmlContent = htmlContent.replaceAll('$fontSize', reportConfig.fontSize);
   htmlContent = htmlContent.replaceAll('$wxh', reportConfig.logoConfig.wxh);
-  htmlContent = htmlContent.replaceAll('$spaceLeft', reportConfig.logoConfig.spaceLeft);
-  htmlContent = htmlContent.replaceAll('$signatureHeigh', reportConfig.signatureHeigh);
+  htmlContent = htmlContent.replaceAll(
+    '$spaceLeft',
+    reportConfig.logoConfig.spaceLeft
+  );
+  htmlContent = htmlContent.replaceAll(
+    '$signatureHeigh',
+    reportConfig.signatureHeigh
+  );
   return htmlContent;
-}
+};
 
-exports.generateSingleFile = async (file, data, outputPath = `${__dirname}/../public/paymentReceipt.pdf`) => {
+exports.generateSingleFile = async (
+  file,
+  data,
+  outputPath = `${__dirname}/../public/paymentReceipt.pdf`
+) => {
   const { name: employeeName, date: printDate } = data;
   let workbook;
   try {
@@ -96,13 +122,18 @@ exports.generateSingleFile = async (file, data, outputPath = `${__dirname}/../pu
     workbook = XLSX.readFile(`${__dirname}/../uploads/reportMonthly.xlsx`);
   }
   const sheet_name_list = workbook.SheetNames;
-  const xlDataArr = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+  const xlDataArr = XLSX.utils.sheet_to_json(
+    workbook.Sheets[sheet_name_list[0]]
+  );
   const date = formatDate(printDate);
 
   const employeeInfo = xlDataArr.filter((xlData) => {
-    const xlData_date = excelDateToStringDateFormat(xlData['Ngày'], 'mm-dd-yyyy');
+    const xlData_date = excelDateToStringDateFormat(
+      xlData['Ngày'],
+      'mm-dd-yyyy'
+    );
     const xlData_name = xlData['Tên nhân viên'];
-    return (xlData_date === date && xlData_name === employeeName)
+    return xlData_date === date && xlData_name === employeeName;
   });
 
   // Get the data from Excel file and modify it
@@ -117,4 +148,4 @@ exports.generateSingleFile = async (file, data, outputPath = `${__dirname}/../pu
   await convertHTML2PDF(template, dataRaw, outputPath);
 
   console.log('😎 Sylitas | Generate PDF successfully');
-}
+};
